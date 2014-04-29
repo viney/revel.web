5826920bac8314a018aaff7bc34236568c5ed57d
viney <viney.chow@gmail.com>
2014-04-29 14:39:19 +0800

    add start bash

---
+++ src/start.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+PWDDIR="$(dirname "`readlink -f $0`")"
+PARENTDIR="$(dirname "$PWDDIR")"
+PROJECT=wechat
+
+PIDDIR=$PARENTDIR/pid
+LOGDIR=$PARENTDIR/log
+
+mkdir -p $PIDDIR
+mkdir -p $LOGDIR
+
+
+echo ------$PROJECT_NAME stop------
+# kill pid
+for file in `ls $PIDDIR`
+do
+    kill -9 `cat $PIDDIR/$file 2>/dev/null` 2>/dev/null
+    rm $PIDDIR/$file 2>/dev/null
+done
+
+echo ------$PROJECT_NAME start------
+# run project
+for pro in `ls $PWDDIR`
+do
+    if [ $pro = $PROJECT ];then
+        revel run $PROJECT 2>>$LOGDIR/$PROJECT.log &
+        # store pid
+        PID=$!
+        ps -p $PID >/dev/null
+        echo $! > $PIDDIR/$PROJECT.pid
+        break
+    fi
+done
